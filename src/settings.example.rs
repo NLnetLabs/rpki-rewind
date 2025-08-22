f56333194fcbeebb44c56d9fcd4d0ef0900b26ef
Koen <koen@nlnetlabs.nl>
2025-08-22 16:42:40 +0200

    Change settings file to settings example file

--- src/settings.example.rs
+++ src/settings.example.rs
@@ -7,3 +7,11 @@ pub const UPDATE_RRDP: u32 = 600;
 
 /// Update frequency for the endpoint in seconds
 pub const UPDATE_NOTIFICATION: u32 = 60;
+
+/// The database connection details
+pub const DB_POOL: u32  = 10;
+pub const DB_USER: &str = "";
+pub const DB_PASS: &str = "";
+pub const DB_HOST: &str = "localhost";
+pub const DB_NAME: &str = "";
+
